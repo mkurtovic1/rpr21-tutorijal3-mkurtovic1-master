dd0e21a1b1e8fbbd3bb26d97402980153424496f
Mirela <mkurtovic@etf.unsa.ba>
2021-11-01 13:29:46 +0100

    lll

--- src/ba/unsa/etf/rpr/tutorijal_3/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal_3/TelefonskiBroj.java
@@ -2,10 +2,7 @@ package ba.unsa.etf.rpr.tutorijal_3;
 
 public abstract class TelefonskiBroj implements Comparable<TelefonskiBroj> {
     public abstract String ispisi();
-    @Override
-    public int hashCode(){
-        return
-    }
+
     @Override
     public int compareTo(TelefonskiBroj telefonskiBroj){
         return ispisi().compareTo(telefonskiBroj.ispisi());
